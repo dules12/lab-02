3aff2a5be6c4eb0e39cb43ebc4d84384050edadd
dules12 <danielgeo@comcast.net>
2018-11-14 13:08:06 -0800

    have a good start on the js.. trying to get images to render properly

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # lab-02
 1) collaborated with Adam Wittnam
 
-Number and name of feature: 1, Display Images
+Number and name of feature: #1, Display Images
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: 2 ish hours
 
-Start time: _____
+Start time: 11:30
 
 Finish time: _____
 
---
+++ js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+//DOG CONSTRUCTOR
+function Creature(creature) {
+  this.title = creature.title;
+  this.image_url = creature.image_url;
+  this.description = creature.description;
+  this.keyword = creature.keyword;
+  this.horns = creature.horns;
+}
+Creature.allCreatures = [];
+//---------------------
+//ADDING RENDER METHOD USING DOG.PROTOTYPE
+Creature.prototype.render = function() {
+  $('main').append('<div class="clone"></div>'); //APPENDS A DIV CLASS=CLONE TO MAIN (HTML TAG)
+  let creatureClone = $('div[class="clone"]'); //HTML ELEMENT
+  let creatureHtml = $('#creature-template').html(); //CONTENTS (INNERHTML) OF DOG-TEMPLATE
+  creatureClone.html(creatureHtml) //CONTENTS OF DOGCLONE IS NOW DOGHTML
+  creatureClone.find('h2').text(this.title); //RETURNS FIRST H2 TAG AVAILABLE IN DOG-TEMPLATE CLASS AND SETS THE TEXT TO THIS.NAME
+  creatureClone.find('img').attr('src', this.image_url); //RETURNS FIRST IMG TAG AVAILABLE IN DOG-TEMPLATE CLASS AND SETS THE TEXT TO THIS.NAME
+  creatureClone.find('p').text(this.description);
+  creatureClone.find('p').text(this.keyword);
+  creatureClone.find('p').text(this.horns);
+  creatureClone.removeClass('clone'); //REMOVES #DOG-TEMPLATE CLASS
+  creatureClone.attr('class', this.name); //SETTING DOGCLONE ATTR TO THIS.NAME
+}
+Creature.readJson = () => {
+  $.get('../data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(obj => {
+        Creature.allCreatures.push(new Creature(obj)); //PUSHES DOGS TO ALLDOGS[]
+      })
+    })
+    .then(Creature.loadCreatures)
+}
+Creature.loadCreatures = () => { //CALLS THE RENDER FUNCTION FOR EACH DOG OBJ
+  Creature.allCreatures.forEach(creature => creature.render())
+}
+$(() => Creature.readJson()); //CALLS READJSON
