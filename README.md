8bef3e0c9ed140fd5a691b1314753be5a8d62d12
dules12 <danielgeo@comcast.net>
2018-11-14 13:38:58 -0800

    finished #1 and started to try #2

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ Start time: 11:30 am
 Finish time:1:15 pm
 
 Actual time needed to complete: 1:45
+
+
+Number and name of feature: #2, Filter Images
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 1:25 am
+
+Finish time:
+
+Actual time needed to complete: 
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-//DOG CONSTRUCTOR
+//Creature CONSTRUCTOR
 function Creature(creature) {
   this.title = creature.title;
   this.image_url = creature.image_url;
@@ -9,29 +9,29 @@ function Creature(creature) {
 }
 Creature.allCreatures = [];
 //---------------------
-//ADDING RENDER METHOD USING DOG.PROTOTYPE
+//ADDING RENDER METHOD USING Creature.PROTOTYPE
 Creature.prototype.render = function() {
   $('main').append('<div class="clone"></div>'); //APPENDS A DIV CLASS=CLONE TO MAIN (HTML TAG)
   let creatureClone = $('div[class="clone"]'); //HTML ELEMENT
-  let creatureHtml = $('#photo-template').html(); //CONTENTS (INNERHTML) OF DOG-TEMPLATE\
-  creatureClone.html(creatureHtml) //CONTENTS OF DOGCLONE IS NOW DOGHTML
-  creatureClone.find('h2').text(this.title); //RETURNS FIRST H2 TAG AVAILABLE IN DOG-TEMPLATE CLASS AND SETS THE TEXT TO THIS.NAME
-  creatureClone.find('img').attr('src', this.image_url); //RETURNS FIRST IMG TAG AVAILABLE IN DOG-TEMPLATE CLASS AND SETS THE TEXT TO THIS.NAME
+  let creatureHtml = $('#photo-template').html(); //CONTENTS (INNERHTML) OF Photo-TEMPLATE\
+  creatureClone.html(creatureHtml) //CONTENTS OF CreatureHTML IS NOW Creatureclone
+  creatureClone.find('h2').text(this.title); //RETURNS FIRST H2 TAG AVAILABLE IN CLASS AND SETS THE TEXT TO THIS.NAME
+  creatureClone.find('img').attr('src', this.image_url); //RETURNS FIRST IMG TAG AVAILABLE IN PHOTO-TEMPLATE CLASS AND SETS THE TEXT TO THIS.NAME
   creatureClone.find('p').text(this.description);
 
-  creatureClone.removeClass('clone'); //REMOVES #DOG-TEMPLATE CLASS
-  creatureClone.attr('class', this.name); //SETTING DOGCLONE ATTR TO THIS.NAME
+  creatureClone.removeClass('clone'); //REMOVES #PHTO-TEMPLATE CLASS
+  creatureClone.attr('class', this.name); //SETTING CreatureClone ATTR TO THIS.NAME
 }
 Creature.readJson = () => {
   $.get('../data/page-1.json', 'json')
     .then(data => {
       data.forEach(obj => {
-        Creature.allCreatures.push(new Creature(obj)); //PUSHES DOGS TO ALLDOGS[]
+        Creature.allCreatures.push(new Creature(obj)); //PUSHES creatures TO allCreatures[]
       })
     })
     .then(Creature.loadCreatures)
 }
-Creature.loadCreatures = () => { //CALLS THE RENDER FUNCTION FOR EACH DOG OBJ
+Creature.loadCreatures = () => { //CALLS THE RENDER FUNCTION FOR EACH Creature OBJ
   Creature.allCreatures.forEach(creature => creature.render())
 }
 $(() => Creature.readJson()); //CALLS READJSON
